2bcacd6e5433ba620c9eb5bae4c0a60fa6bad73d
abauna <jacksonickolay@gmail.com>
2022-08-02 10:31:22 -0300

    add codes

--- main.py
+++ main.py
@@ -5,21 +5,48 @@
 
 
 import tkinter as tk
-
+def salva_emp(a,b):
+   print("nome",a)
+   print("autor",b)
+   dat=1
+   pass
+def salva_dev(a,b):
+   print("aluno",a)
+   print("livro",b)
+   dat = 1
+   pass
+def salva_alu(a,b,c,d,e):
+   print("nomel",a)
+   print("ctr",d)
+   print("telefone", e)
+   print("nome",c )
+   print("autor", b)
+   pass
+def salva_liv(a,b):
+   print("nome",a)
+   print("autor",b)
+   pass
 def listar():
    pass
-def cadlivro():
+def cadlivro(fl,n,t,c):
    newWindow = tk.Toplevel(app)
    labelExample = tk.Label(newWindow, text="Nome")
    labelExample.pack()
    entrada = tk.Entry(newWindow, font="arial 15 bold")
    entrada.pack()
+#   print("nome:",entrada.get)
    label = tk.Label(newWindow, text="autor")
    label.pack()
    entrad = tk.Entry(newWindow, font="arial 15 bold")
    entrad.pack()
-   buttonExample = tk.Button(newWindow, text="proxima",command=newWindow.destroy)
-   buttonExample.pack()
+   #botao = Button(janela, text="Rodar código", command=lambda: querypg(codigo.get()))
+   #botao.grid(column=0, row=2)
+   if(fl):
+      buttonExample = tk.Button(newWindow, text="proxima",command=lambda: salva_liv(entrada.get(),entrad.get()))
+      buttonExample.pack()
+   else:
+      buttonExample = tk.Button(newWindow, text="proxima", command=lambda: salva_alu(entrada.get(), entrad.get(),n,t,c))
+      buttonExample.pack()
    buttonExample = tk.Button(newWindow, text="cancelar", command=newWindow.destroy)
    buttonExample.pack()
    pass
@@ -34,7 +61,7 @@ def emprestar():
    label.pack()
    entrad = tk.Entry(newWindow, font="arial 15 bold")
    entrad.pack()
-   buttonExample = tk.Button(newWindow, text="concluir",command=newWindow.destroy)
+   buttonExample = tk.Button(newWindow, text="concluir",command=lambda: salva_emp(entrada.get(),entrad.get()))
    buttonExample.pack()
    buttonExample = tk.Button(newWindow, text="cancelar", command=newWindow.destroy)
    buttonExample.pack()
@@ -51,7 +78,7 @@ def devolver():
    entrad.pack()
    label = tk.Label(newWindow, text="data")
    label.pack()
-   buttonExample = tk.Button(newWindow, text="concluir",command=newWindow.destroy)
+   buttonExample = tk.Button(newWindow, text="concluir",command=lambda: salva_dev(entrada.get(),entrad.get()))
    buttonExample.pack()
    buttonExample = tk.Button(newWindow, text="cancelar", command=newWindow.destroy)
    buttonExample.pack()
@@ -71,7 +98,7 @@ def cadaluno():
    labele.pack()
    entra = tk.Entry(newWindow, font="arial 15 bold")
    entra.pack()
-   buttonExample = tk.Button(newWindow, text="proxima", command=cadlivro)
+   buttonExample = tk.Button(newWindow, text="proxima", command=lambda :cadlivro(False,entrada.get(),entrad.get(),entra.get()))
    buttonExample.pack()
    buttonExample = tk.Button(newWindow, text="cancelar", command=newWindow.destroy)
    buttonExample.pack()
@@ -85,7 +112,7 @@ def createNewWindow():
     pass
 app = tk.Tk()
 buttona = tk.Button(app,text="Cadastrar aluno",command=cadaluno)
-buttonb = tk.Button(app,text="Cadastar o livro",command=cadlivro)
+buttonb = tk.Button(app, text="Cadastar o livro", command=lambda: cadlivro(True,0,0,0))
 buttonc = tk.Button(app,text="emprestar",command=emprestar)
 buttond = tk.Button(app,text="devolver",command=devolver)
 buttone = tk.Button(app,text="listar",command=listar)
